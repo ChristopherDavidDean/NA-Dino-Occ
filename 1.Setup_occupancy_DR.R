c73b1d65a138557e55af7d7754ad409d78af5cfb
Christopher Dean <christopherdaviddean@gmail.com>
2022-08-25 17:44:46 +0100

    Time Binning additions
    Added code to allow for different time binning methods, using the bin_time() function. Needs tidying, but is now working.

--- 1.Setup_occupancy_DR.R
+++ 1.Setup_occupancy_DR.R
@@ -10,13 +10,24 @@
 
 #============================================== INITIAL SETUP ===============================================
 
-# Broad PBDB download: https://paleobiodb.org/data1.2/occs/list.csv?base_name=tetrapoda&taxon_reso=genus&pres=regular&interval=Campanian,Maastrichtian&cc=NOA&envtype=terr&show=full,strat,lith,env
+# Broad PBDB download: http://paleobiodb.org/data1.2/occs/list.csv?datainfo&rowcount&base_name=Tetrapoda&interval=Campanian,Maastrichtian&cc=NOA,^MX&envtype=terr&show=full,strat,lith,env,timebins,timecompare,ref
 
 # Set working directory
-setwd("C:/Users/deancd/Documents/RESEARCH/PROJECTS/DINO_RANGE/NA-Dino-Occ/") # Set your working directory
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # Set your working directory
 
-# Load master spreadsheet
-master.occs <- read.csv("Data/Occurrences/Master_spreadsheet_v1_051219.csv")
+# Load master spreadsheets and organise
+master.occs <- read.csv("Data/Occurrences/occs_all_stages_tetrapoda_28_06_2022.csv", skip = 20)
+master.occs <- master.occs %>%
+  filter(max_ma < 85) # Remove older occurrences than we are looking at
+
+formations <- read.csv("Data/Occurrences/Formations.csv") # Load formations
+formations <- formations[,1:11] # Remove additional columns
+formations <- formations[order(formations$Formation),] # Reorganise formations
+formations$forbinning <- 1:nrow(formations) # Provide if for formations
+formations$Range <- formations$max_age - formations$min_age # Calculate formation range
+formations$Diversity <- 0 # Add in dummy variables to ensure code works (sorry!)
+formations$Occurrences <- 0 # Add in dummy variables to ensure code works (sorry!)
+colnames(formations)[1] <- "formation" # Change to allow for further analysis
 
 #==== If first time using: ====
 # Make vector of package names
@@ -38,6 +49,7 @@ library(maps)
 library(maptools)
 library(parallel)
 library(tibble)
+library(divDyn)
 
 #==== if not: ====
 # Load in Functions
@@ -49,10 +61,10 @@ res <- 0.1
 e <- extent(-155, -72, 22.5, 73)
 
 #==== Manually setup extent for figures ====
-#maxLat <- round_any((max(Morrison_test$lat) + 7), 0.5)  #get value for defining extent, and increase by x for visualisation purposes
-#minLat <- round_any((min(Morrison_test$lat) - 7), 0.5)  #get value for defining extent, and increase by x for visualisation purposes
-#maxLng <- round_any((max(Morrison_test$lng) + 10), 0.5)  #get value for defining extent, and increase by x for visualisation purposes
-#minLng <- round_any((min(Morrison_test$lng) - 10), 0.5) #get value for defining extent, and increase by x for visualisation purposes
+#maxLat <- round_any((max(occurrence_dataset$lat) + 7), 0.5)  #get value for defining extent, and increase by x for visualisation purposes
+#minLat <- round_any((min(occurrence_dataset$lat) - 7), 0.5)  #get value for defining extent, and increase by x for visualisation purposes
+#maxLng <- round_any((max(occurrence_dataset$lng) + 10), 0.5)  #get value for defining extent, and increase by x for visualisation purposes
+#minLng <- round_any((min(occurrence_dataset$lng) - 10), 0.5) #get value for defining extent, and increase by x for visualisation purposes
 #e <<- extent(minLng, maxLng, minLat, maxLat)
 
 #==== Convert rasters to desired resolution ====
@@ -60,11 +72,54 @@ source("0.Format_data_DR.R") # Import extent and run cleaning/import of raster d
 # but will automatically update rasters so that they are of the desired resolution and place them in the appropriate folder
 # for running the next steps. 
 
-#=============================================== CAMPANIAN DATA SETUP ===============================================
+#=============================================== TIME BINNING =======================================================
 
-# Read in files
-camp.occs <- read.csv("Data/Occurrences/Camp_data_V1_Species_removed.csv", stringsAsFactors = FALSE) # Load in occurrences
+# Standard Bin setup - trim to fit relevant time frame. 
+data(stages)
+stages <- stages[80:81,] # Set stages to range from Campanian to Maastrichtian
 
+# Attach new age constraints from formations data
+master.occs$old_max_ma <- master.occs$max_ma # Move old values to legacy
+master.occs$old_min_ma <- master.occs$min_ma
+test <- merge(master.occs, formations, by = "formation", all = T) # Merge occurrences with formation ages
+test <- test %>%
+  filter(is.na(occurrence_no) == F) # Remove non-occurrences
+test$max_ma <- test$max_age # Add new ages to ma_ma and min_ma
+test$min_ma <- test$min_age
+master.occs <- test[,1:140] # Remove extra columns from final dataset.
+
+#==== STAGE ====
+bins <- stages
+master.occs.stage <- bin_time(master.occs, bins, method = "majority")
+         
+#==== SUB STAGE ====
+bins <- 
+master.occs.stage <- bin_time(master.occs, bins, method = "majority")
+
+#==== FORMATION BINS ====
+formations <- formations %>%
+  dplyr::filter(min_age < 84) # Limit what formations are included
+bin_limits <- c(2.5, max(formations$max_age), 66) # Set user defined bin size - change the first number to vary resolution in graphs.
+Scoring_Grid_1(formations = formations, res = 0.01)
+#Scoring_Grid_2(formations = formations, res = 0.01)
+newBins(score_grid = score_grid, formations = formations, bin_limits = bin_limits, allbins = allbins, stages = stages, smallamalg = TRUE)
+
+bins <- binlist %>% # Remove non-useful bins outside of range
+  filter(bottom < 84)
+colnames(bins) <- c("bin", "min_ma", "max_ma", "mid_ma") # Rename bin names to match across
+bins[1,2] <- 66 # Cap youngest bin at 66 Ma. 
+
+master.occs <- master.occs %>% 
+  filter(is.na(max_ma) == F) %>% # Remove occurrences without a max age (occurrences which can't be matched to a formation)
+  filter(max_ma <84.6) # Remove occurrences older than necessary.
+
+# If using majority method, all occurrences with minimum age of <66 Ma must be capped to 66 Ma, otherwise bin_time() breaks.
+master.occs$min_ma[master.occs$min_m<66] <- 66
+
+# Bin occurrences
+master.occs.form <- bin_time(master.occs, bins, method = 'majority') 
+
+ #=============================================== CAMPANIAN DATA SETUP ===============================================
 
 #==== Visualise grid cells for collections and occurrences ====
 camp.colls <- camp.occs %>% # Make unique collections for visualisation
